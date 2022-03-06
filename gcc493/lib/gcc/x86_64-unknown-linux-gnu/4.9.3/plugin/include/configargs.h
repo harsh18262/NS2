b192420af8effa688bebbd9be70e7ccfaf03eb7e
Harshwardhan Mehrotra <harshwardhanmehrotra@gmail.com>
2022-03-06 19:05:09 +0530

    add the write gcc version

---
+++ gcc493/lib/gcc/x86_64-unknown-linux-gnu/4.9.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.9.3/configure --prefix=/usr/local/gcc493 --program-suffix=49 --enable-languages=c,c++ --disable-libstdcxx-pch --disable-multilib";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
